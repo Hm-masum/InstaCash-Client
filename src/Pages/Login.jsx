c3ca34ef07d75f458486d6e054f7e411a5a1c35b
Hm-masum <hhmasum88@gmail.com>
2024-07-16 13:06:38 +0600

    login page commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,67 @@
+import { Link } from "react-router-dom";
+import imgBg from '../assets/imgBg.png'
 
 const Login = () => {
     return (
-        <div>
-            
+      <div className="flex md:flex-row flex-col items-center justify-center px-3 md:px-24">
+        <div className="md:w-1/2">
+          <img className="w-[600px] md:h-[600px]" src={imgBg} alt="" />
         </div>
+
+        <div className="md:w-1/2 px-2 md:px-14 space-y-4">
+          <h2 className="text-4xl font-semibold text-center pb-3">
+            Login Please!
+          </h2>
+
+          <form >
+            <div className="space-y-4">
+              <div>
+                <label className="block mb text-sm">Mobile Number</label>
+                <div className="mt-2 relative">
+                  <input
+                    type="number"
+                    name="mobile"
+                    required
+                    placeholder="Enter Mobile Number"
+                    className="w-full p-3 border rounded-md border-gray-400 text-gray-900"
+                  />
+                </div>
+              </div>
+              
+              <div>
+                <label className="block mb text-sm">PIN Number</label>
+                <div className="mt-2 relative">
+                  <input
+                    type="number"
+                    name="pin"
+                    required
+                    placeholder="Enter Pin Number"
+                    className="w-full p-3 border rounded-md border-gray-400 text-gray-900"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="bg-purple-700 font-semibold w-full rounded-md text-center py-3 text-white"
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <div>
+            <p className="text-lg">
+              Are you new ?{" "}
+              <Link className="font-semibold text-purple-600" to={"/Register"}>
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     );
 };
 
